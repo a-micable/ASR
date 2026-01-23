b0b33beee2ae5e25d1d09e4beb8011c97f3a1c9c
a-micable <a.micable@dev.com>
2026-01-23 18:00:16 +0000

    test(export): test records_to_json preserves Ethiopic characters

--- tests/test_export.py
+++ tests/test_export.py
@@ -145,3 +145,4 @@ class TestSegmentsToJSONL:
         assert segments_to_jsonl([]) == ""
 # Ethiopic characters (ሰላም ዓለም) preserved verbatim in all export formats
 # first segment has index 1 in SRT output
+# json.loads(records_to_json([r]))[0]['transcription'] == '\u1230\u1208\u121d \u12d3\u1208\u121d'
